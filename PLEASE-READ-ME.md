1f93e33cb8e9983c307649431eeab0992071d777
Gabe-riel01 <r.gabriel.maize.am@gmail.com>
2025-03-13 20:41:12 -0500

    please please please

---
+++ PLEASE-READ-ME.md
@@ -0,0 +1,4 @@
+To run this project, run the run-this.sh file as it will start both the server and the react project at the same time
+
+If you have issues running run-this.sh, run the following command  "chmod +x run-this.sh" in your terminal to give the
+sh file execute permissions.
--- run-this.sh
+++ run-this.sh
@@ -1,12 +1,14 @@
 # Navigate to FastAPI project and start the API
 echo "Starting FastAPI server..."
-cd /climate-proj-backend || exit 1  
-source venv/bin/activate 
+cd climate-proj-backend || exit 1  
+source .venv/bin/activate 
 uvicorn run:app --reload --host 0.0.0.0 --port 8000 & 
+
+cd ".."
 
 # Navigate to React project and start React server
 echo "Starting React app..."
-cd /climate-proj-ui/climate-proj
+cd climate-proj-ui/climate-proj
 npm start
 
 wait
